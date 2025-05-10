6c482df70604e399d916eb94d6154914fe9aed03
tslaird <tyler.laird@nist.gov>
2025-05-09 20:19:21 -0400

    fix: fixed batch script

--- batch_run.sh
+++ batch_run.sh
@@ -3,7 +3,7 @@
 # List of directories to process
 #DIRS="dir1 dir2 dir3"
 # Or use a glob pattern like this:
-DIRS="input/EEE"
+DIRS="input/EEE input/EHV input/Neospora_hughesi input/Sarcocystis_neurona input/Theileria_equi"
 
 # Check if DIRS is empty
 if [ -z "$DIRS" ]; then
@@ -18,6 +18,9 @@ for dir in $DIRS; do
     cd "$dir"
     pwd
     ALL_FA=$(ls fasta/all*.fa 2>/dev/null | head -n 1)
+    ALL_FA_BASE=${ALL_FA##*/}
+    echo $ALL_FA_BASE
+    echo $ALL_FA
     YAML_FILE=$(ls *yaml 2>/dev/null | head -n 1)
     #echo "ALL_FA: $ALL_FA"
     #echo "YAML_FILE: $YAML_FILE"
@@ -26,19 +29,19 @@ for dir in $DIRS; do
       SENTINEL_FILE="${ALL_FA}.processed"
       if [ ! -f "$SENTINEL_FILE" ] || [ "$ALL_FA" -nt "$SENTINEL_FILE" ]; then
         # convert fasta headers and seq to unique hash
-        ../../scripts/hash_fasta.py -i $ALL_FA -o ${ALL_FA%.*##*/}.hash.fa -m ${ALL_FA%.*##*/}.hashmap
+        ../../scripts/hash_fasta.py -i $ALL_FA -o ${ALL_FA_BASE}.hash.fa -m ${ALL_FA_BASE}.hashmap
         # edit the config yaml
         #sed -i "/virus:\s*-/s/.*/  - ${ALL_FA%.fa}.hash/" $YAML_FILE
         # run snakemake multiPrime workflow
         #TODO figure out how to override and set config file values
-        snakemake --configfile $YAML_FILE -s ../../multiPrime.py --cores 4 --resources mem_mb=60000 -C input_dir=$FULL_DIR results_dir=$FULL_DIR/results log_dir=$FULL_DIR/logs virus=${ALL_FA%.*##*/}.hash && touch .snakemake.done
+        snakemake --configfile $YAML_FILE -s ../../multiPrime.py --cores 4 --resources mem_mb=60000 -C input_dir=$FULL_DIR results_dir=$FULL_DIR/results log_dir=$FULL_DIR/logs virus=${ALL_FA_BASE}.hash && touch .snakemake.done
         if [ -f "results/Core_primers_set/BWT_coverage/core_final_maxprimers_set.out" ] && [ -f ".snakemake.done" ]; then
             # extract amplicons
-            ../../scripts/extractAmplicon.py -t results/Core_primers_set/BWT_coverage/core_final_maxprimers_set.out -f ${ALL_FA%.*##*/}.hash.fa -o ${ALL_FA%.*##*/}_ampicons_hash.fasta
+            ../../scripts/extractAmplicon.py -t results/Core_primers_set/BWT_coverage/core_final_maxprimers_set.out -f ${ALL_FA_BASE}.hash.fa -o ${ALL_FA_BASE}_ampicons_hash.fasta
             # map back to original headers
-            ../../scripts/map_hash_fasta.py -m ${ALL_FA%.*##*/}.hashmap -i ${ALL_FA%.*##*/}_ampicons_hash.fasta -o ${ALL_FA%.*##*/}_ampicons.fasta
+            ../../scripts/map_hash_fasta.py -m ${ALL_FA_BASE}.hashmap -i ${ALL_FA_BASE}_ampicons_hash.fasta -o ${ALL_FA_BASE}_ampicons.fasta
             # make final output table
-            ../../scripts/build_table.py -t results/Core_primers_set/BWT_coverage/core_final_maxprimers_set.out -p results/Core_primers_set/core_final_maxprimers_set.fa -g results/Total_fa/${ALL_FA%.*##*/}.hash.format.fa -o ${ALL_FA%.*##*/}_primer_amplicon_table.csv -m ${ALL_FA%.*##*/}.hashmap
+            ../../scripts/build_table.py -t results/Core_primers_set/BWT_coverage/core_final_maxprimers_set.out -p results/Core_primers_set/core_final_maxprimers_set.fa -g results/Total_fa/${ALL_FA_BASE}.hash.format.fa -o ${ALL_FA_BASE}_primer_amplicon_table.csv -m ${ALL_FA_BASE}.hashmap
             # Update sentinel file
             touch "$SENTINEL_FILE"
         fi
